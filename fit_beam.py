25212e922a0a3572daf47583bc8698f03baa6cd4
Tomas Cassanelli <tcassanelli@mpifr-bonn.mpg.de>
2017-03-08 15:29:49 +0100

    Selection of pedestal illumination

--- fit_beam.py
+++ fit_beam.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.constants import c as light_speed
 from main_functions import angular_spectrum, wavevector_to_degree
 from scipy.optimize import least_squares
@@ -6,6 +7,7 @@ from astropy.io import fits
 from scipy.interpolate import RegularGridInterpolator
 from astropy.io import ascii
 from astropy.table import Table
+from plot_routines import plot_beam, plot_data, plot_phase
 import os
 import time
 import ntpath
@@ -13,7 +15,7 @@ import ntpath
 
 def find_name_path(path):
     head, tail = ntpath.split(path)
-    return tail or ntpath.basename(head)
+    return head, tail
 
 
 def residual(
@@ -69,17 +71,18 @@ def residual(
 
 
 # Insert path for the fits file with pre-calibration
-def fit_beam(path_fits, order, fit_illum_params):
+def fit_beam(pathfits, order, fit_illum_params, illum_func):
 
     start_time = time.time()
 
-    print('####### Fit Beam pattern #######')
+    print('\n')
+    print('####### OOF FIT BEAM PATTERN #######')
     print('\n')
 
     print('... Reading data ...')
     print('\n')
     # Opening fits file with astropy
-    hdulist = fits.open(path_fits)
+    hdulist = fits.open(pathfits)
 
     # Observation frequency
     frequency = hdulist[0].header['FREQ']  # Hz
@@ -88,17 +91,16 @@ def fit_beam(path_fits, order, fit_illum_params):
     beam_data = [hdulist[i].data['fnu'] for i in range(1, 4)][::-1]
     u_data = [hdulist[i].data['DX'] for i in range(1, 4)][::-1]
     v_data = [hdulist[i].data['DY'] for i in range(1, 4)][::-1]
-    d_z = [hdulist[i].header['DZ'] for i in range(1, 4)][::-1]
+    d_z_m = [hdulist[i].header['DZ'] for i in range(1, 4)][::-1]
 
     # Permuting the position to provide same as main_functions
     beam_data.insert(1, beam_data.pop(2))
     u_data.insert(1, u_data.pop(2))
     v_data.insert(1, v_data.pop(2))
-    d_z.insert(1, d_z.pop(2))
+    d_z_m.insert(1, d_z_m.pop(2))
 
     # d_z is given in units of wavelength (m/m)
-    d_z = np.array(d_z)
-    d_z *= 2 * np.pi / wavelength  # convert to radians
+    d_z = np.array(d_z_m) * 2 * np.pi / wavelength  # convert to radians
 
     # Beam normalisation
     beam_data_norm = [beam_data[i] / beam_data[i].max() for i in range(3)]
@@ -113,21 +115,20 @@ def fit_beam(path_fits, order, fit_illum_params):
     z_coeff_to_fit = (n + 1) * (n + 2) // 2 - 1   # to give knl = 0
 
     if fit_illum_params:
-        params_init = np.array([0.01, .9, 0, 0] + [0.1] * z_coeff_to_fit)
+        params_init = np.array([0.01, -10, 0, 0] + [0.1] * z_coeff_to_fit)
         # amp, sigma_r, x0, y0, K(l,m)
         # Giving an initial value of 0.1 for each coeff
 
         params_bounds = np.array(
-            [[0, 1], [0.9, 1], [-1e-8, 1e-8], [-1e-8, 1e-8], [-2, 2]] +
-            [[-1, 1]] * (z_coeff_to_fit - 1))
+            [[0, 1], [-25, -8], [-1e-3, 1e-3], [-1e-3, 1e-3]] + [[-20, 20]] *
+            2 + [[-1, 1]] * (z_coeff_to_fit - 2))
 
     else:
         params_init = np.array([0.01] + [0.1] * z_coeff_to_fit)
         # amp, K(l,m)
         # Giving an initial value of 0.1 for each coeff
 
-        params_bounds = np.array(
-            [[0, 1], [-2, 2]] + [[-1, 1]] * (z_coeff_to_fit - 1))
+        params_bounds = np.array([[0, 1]] + [[-16, 16]] * z_coeff_to_fit)
 
     print('... Starting fit ...')
     print('\n')
@@ -143,17 +144,15 @@ def fit_beam(path_fits, order, fit_illum_params):
             y_cal,
             d_z,
             wavelength,
-            'gauss',  # illumination
+            illum_func,  # illumination
             fit_illum_params),  # True or False
         bounds=tuple(
             [params_bounds[:, 0].tolist(), params_bounds[:, 1].tolist()]),
         method='trf',
         verbose=2,
-        # max_nfev=50
+        # max_nfev=1
         )
 
-    print('\n')
-    print(res_lsq.message)
     print('\n')
 
     if fit_illum_params:
@@ -168,32 +167,91 @@ def fit_beam(path_fits, order, fit_illum_params):
     L = np.array(ln)[:, 0]
     N = np.array(ln)[:, 1]
 
-    params_names = ['Illum_amp', 'sigma_r', 'x_0', 'y_0', 'K(0,0)']
+    params_names = ['Illum_amp', 'c_db', 'x_0', 'y_0', 'K(0,0)']
     for i in range(z_coeff_to_fit):
         params_names.append('K(' + str(L[i]) + ',' + str(N[i]) + ')')
 
     table = Table(
         {'Parameter': params_names, 'Fit': params_solution,
             'Initial guess': params_init},
-            names=['Parameter', 'Fit', 'Initial guess'])
+        names=['Parameter', 'Fit', 'Initial guess'])
+
     print(table)
     print('\n')
 
-    name_file = find_name_path(path_fits)[:-5]
+    name_file = find_name_path(pathfits)[1][:-5]
+    name_dir = find_name_path(pathfits)[0] + '/OOF_out'
 
-    if not os.path.exists(find_name_path(name_file)):
-        os.makedirs(find_name_path(name_file))
+    if fit_illum_params:
+        title_beam = name_file + ' fitted beam $n=' + str(n) + '$'
+        title_phase = name_file + ' Aperture phase distribution $n=' + str(n) + '$'
+    else:
+        illum_note = ' (no fitted coeff. $I(x,y)$)'
+        title_beam = name_file + ' fitted beam $n=' + str(n) + '$' + illum_note
+        title_phase = name_file + ' Aperture phase distribution $n=' + str(n) + '$' + illum_note
 
-    name_to_save = name_file + '/' + name_file + '_parfit.dat'
-    data_to_save = [params_names, params_solution, params_init]
+    fig_data = plot_data(
+        u_b_data=u_data,
+        v_b_data=v_data,
+        beam_data=beam_data,
+        d_z_m=d_z_m,
+        title=name_file + ' observed beam',
+        rad=False
+        )
+
+    fig_beam = plot_beam(
+        params=params_solution,
+        title=title_beam,
+        x=x_cal,
+        y=y_cal,
+        d_z_m=d_z_m,
+        lam=wavelength,
+        illum=illum_func,
+        rad=False
+        )
+
+    fig_phase = plot_phase(
+        params=params_solution,
+        d_z_m=d_z_m[2],  # only one function for the three beam maps
+        title=title_phase,
+        notilt=True
+        )
+
+    if not os.path.exists(name_dir):
+        os.makedirs(name_dir)
+
+    params_to_save = [params_names, params_solution, params_init]
+    info_to_save = [
+        [name_file], [d_z_m[0]], [d_z_m[1]], [d_z_m[2]], [wavelength],
+        [n], [illum_func]]
 
     # Storing files in directory
     ascii.write(
-        data_to_save, name_to_save, names=['parname', 'parfit', 'parinit'])
+        params_to_save, name_dir + '/fitpar_n' + str(n) + '.dat',
+        names=['parname', 'parfit', 'parinit'])
 
-    print('####### %s mins #######' % str((time.time() - start_time) / 60))
+    ascii.write(
+        info_to_save, name_dir + '/fitinfo_n' + str(n) + '.dat',
+        names=['name', 'd_z-', 'd_z0', 'd_z+', 'wavelength', 'n', 'illum'], fast_writer=False)
+
+    print('... Making plots ...')
+    print('\n')
+
+    fig_data.savefig(name_dir + '/obsbeam.pdf')
+    fig_beam.savefig(name_dir + '/fitbeam_n' + str(n) + '.pdf')
+    fig_phase.savefig(name_dir + '/fitphase_n' + str(n) + '.pdf')
+
+    print('###### %s mins ######' % str((time.time() - start_time) / 60))
+
+    plt.show()
 
 
 if __name__ == "__main__":
 
-    fit_beam('../test_data/3260_3C84_32deg_SB-002/3260_3C84_32deg_SB.fits', 5, False)
+    # Testing script
+    fit_beam(
+        pathfits='../test_data/3260_3C84_32deg_SB-002/3260_3C84_32deg_SB.fits',
+        order=5,
+        fit_illum_params=True,
+        illum_func='pedestal'
+        )
