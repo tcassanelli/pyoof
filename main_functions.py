1ca5cd4aa243b339595e8c72fef9107769e80e7e
Tomas Cassanelli <tcassanelli@mpifr-bonn.mpg.de>
2017-03-30 15:14:08 +0200

    minor details

--- main_functions.py
+++ main_functions.py
@@ -1,13 +1,14 @@
+# Author: Tomas Cassanelli
 import numpy as np
 from math import factorial as f
 
 # All mathematical function have been adapted for the Effelsber telescope
 
 
-def illumination_pedestal(x, y, i_coeff):
+def illumination_pedestal(x, y, I_coeff):
     """
-    Illumination function, sometimes called amplitude. Represents the
-    distribution of light in the primary reflector.
+    Illumination function parabolic taper on a pedestal, sometimes called
+    amplitude. Represents the distribution of light in the primary reflector.
 
     Parameters
     ----------
@@ -15,57 +16,100 @@ def illumination_pedestal(x, y, i_coeff):
         Grid value for the x variable, same as the contour plot.
     y : ndarray
         Grid value for the x variable, same as the contour plot.
-
+    I_coeff : ndarray
+        List which contains 4 parameters, the illumination amplitude, the
+        illumination taper and the two coordinate offset.
     Returns
     -------
     illumination : ndarray
     """
 
-    amp = i_coeff[0]
-    c_db = i_coeff[1]  # number has to be negative [-8, -20]
-    # Centre illuminationprimary reflector
-    x0 = i_coeff[2]
-    y0 = i_coeff[3]
-
+    amp = I_coeff[0]
+    c_dB = I_coeff[1]  # [dB] Illumination taper it is defined by the feedhorn
+    # Number has to be negative, bounds given [-8, -25], see fit
+    x0 = I_coeff[2]  # Centre illumination primary reflector
+    y0 = I_coeff[3]
 
     pr = 50  # Primary reflector radius
-    # c_db = -20  # [dB] Constant for quadratic model illumination
 
     # Parabolic taper on a pedestal
     n = 2  # Order quadratic model illumination (Parabolic squared)
 
-    c = 10 ** (c_db / 20.)
+    c = 10 ** (c_dB / 20.)
     r = np.sqrt((x - x0) ** 2 + (y - y0) ** 2)
     illumination = amp * (c + (1. - c) * (1. - (r / pr) ** 2) ** n)
+
     return illumination
 
 
-def illumination_gauss(x, y, i_coeff):
+def illumination_gauss(x, y, I_coeff):
+    """
+    Illumination function gaussian.
 
-    amp = i_coeff[0]
-    sigma_r = i_coeff[1]  # illumination taper
+    Parameters
+    ----------
+    x : ndarray
+        Grid value for the x variable, same as the contour plot.
+    y : ndarray
+        Grid value for the x variable, same as the contour plot.
+    I_coeff : ndarray
+        List which contains 4 parameters, the illumination amplitude, the
+        illumination taper and the two coordinate offset.
+    Returns
+    -------
+    illumination : ndarray
+    """
+
+    amp = I_coeff[0]
+    sigma_r = I_coeff[1]  # illumination taper
+
     # Centre illuminationprimary reflector
-    x0 = i_coeff[2]
-    y0 = i_coeff[3]
+    x0 = I_coeff[2]
+    y0 = I_coeff[3]
 
     pr = 50  # Primary reflector radius
 
-    illumination = amp * np.exp(-((x - x0) ** 2 + (y - y0) ** 2) / (2 * (sigma_r * pr) ** 2))
+    illumination = (
+        amp *
+        np.exp(-((x - x0) ** 2 + (y - y0) ** 2) / (2 * (sigma_r * pr) ** 2))
+        )
+
     return illumination
 
 
-def illumination_nikolic(x, y, i_coeff):
+def illumination_nikolic(x, y, I_coeff):
+    """
+    Illumination function used by Bojan Nikolic in his OOF software.
 
-    amp = i_coeff[0]
-    sigma_r = i_coeff[1]  # illumination taper
-    # Centre illuminationprimary reflector
-    x0 = i_coeff[2]
-    y0 = i_coeff[3]
+    Parameters
+    ----------
+    x : ndarray
+        Grid value for the x variable, same as the contour plot.
+    y : ndarray
+        Grid value for the x variable, same as the contour plot.
+    I_coeff : ndarray
+        List which contains 4 parameters, the illumination amplitude, the
+        illumination taper and the two coordinate offset.
+    Returns
+    -------
+    illumination : ndarray
+    """
 
     pr = 50  # Primary reflector radius
+    amp = I_coeff[0]
 
-    illumination = amp * np.exp(-((x - x0) ** 2 + (y - y0) ** 2) * sigma_r / pr ** 2)
+    # illumination taper, different value from gauss illumination
+    sigma_r = I_coeff[1]
+
+    # Centre illuminationprimary reflector
+    x0 = I_coeff[2]
+    y0 = I_coeff[3]
+
+    illumination = (
+        amp * np.exp(-((x - x0) ** 2 + (y - y0) ** 2) * sigma_r / pr ** 2))
+
     return illumination
+
 
 def antenna_shape(x, y):
     """
@@ -81,7 +125,7 @@ def antenna_shape(x, y):
 
     Returns
     -------
-    ant_model : ndarray
+    a_shape : ndarray
     """
 
     pr = 50  # Primary reflector radius
@@ -89,11 +133,12 @@ def antenna_shape(x, y):
     L = 20  # length support structure
     a = 1  # half thickness support structure
 
-    ant_model = np.zeros(x.shape)  # or y.shape same
-    ant_model[(x ** 2 + y ** 2 < pr ** 2) & (x ** 2 + y ** 2 > sr ** 2)] = 1
-    ant_model[(-L < x) & (x < L) & (-a < y) & (y < a)] = 0
-    ant_model[(-L < y) & (y < L) & (-a < x) & (x < a)] = 0
-    return ant_model
+    a_shape = np.zeros(x.shape)  # or y.shape same
+    a_shape[(x ** 2 + y ** 2 < pr ** 2) & (x ** 2 + y ** 2 > sr ** 2)] = 1
+    a_shape[(-L < x) & (x < L) & (-a < y) & (y < a)] = 0
+    a_shape[(-L < y) & (y < L) & (-a < x) & (x < a)] = 0
+
+    return a_shape
 
 
 def delta(x, y, d_z):
@@ -109,7 +154,7 @@ def delta(x, y, d_z):
     y : ndarray
         Grid value for the x variable, same as the contour plot.
     d_z : float
-        Distance between the secondary and primary refelctor.
+        Distance between the secondary and primary refelctor measured in rad.
     Returns
     -------
     delta : ndarray
@@ -121,7 +166,10 @@ def delta(x, y, d_z):
     r = np.sqrt(x ** 2 + y ** 2)  # polar coord. radius
     a = r / (2 * f1)
     b = r / (2 * F)
+
+    # d_z has to be in radians
     delta = d_z * ((1 - a ** 2) / (1 + a ** 2) + (1 - b ** 2) / (1 + b ** 2))
+
     return delta
 
 
@@ -150,7 +198,7 @@ def U(l, n, theta, rho):
     """
 
     if n < 0:
-        print('U(l, n) can only have positive values for n')
+        print('WARNING: U(l, n) can only have positive values for n')
 
     m = abs(l)
     a = (n + m) // 2
@@ -164,19 +212,20 @@ def U(l, n, theta, rho):
     # norm = np.sqrt(2 * (n + 1) / (1 + delta_m0))
 
     R = sum(
-    (-1) ** s * f(n - s) * rho ** (n - 2 * s) / (f(s) * f(a - s) * f(b - s))
-    for s in range(0, b + 1)
-    )
+        (-1) ** s * f(n - s) * rho ** (n - 2 * s) /
+        (f(s) * f(a - s) * f(b - s))
+        for s in range(0, b + 1)
+        )
 
     if l < 0:
-        U = R * np.sin(m * theta)  # * norm
+        U = R * np.sin(m * theta)   #* norm
     else:
-        U = R * np.cos(m * theta)  # * norm
+        U = R * np.cos(m * theta)   #* norm
 
     return U
 
 
-def phi(theta, rho, U_coeff, n):
+def phi(theta, rho, K_coeff):
     """
     Generates a series of Zernike polynomials, the aberration function.
 
@@ -186,7 +235,7 @@ def phi(theta, rho, U_coeff, n):
         Values for the angular component. theta = np.arctan(y / x).
     rho : ndarray
         Values for the radial component. rho = np.sqrt(x ** 2 + y ** 2).
-    U_coeff : ndarray
+    K_coeff : ndarray
         Constants organized by the ln list, which gives the possible values.
     n : int
         It is n >= 0. Determines the size of the polynomial, see ln.
@@ -198,21 +247,24 @@ def phi(theta, rho, U_coeff, n):
     """
 
     # List which contains the allowed values for the U function.
+    n = int((np.sqrt(1 + 8 * K_coeff.size) - 3) / 2)
     ln = [(j, i) for i in range(0, n + 1) for j in range(-i, i + 1, 2)]
     L = np.array(ln)[:, 0]
     N = np.array(ln)[:, 1]
 
+    # Aperture phase distribution function in radians
     phi = sum(
-        U_coeff[i] * U(L[i], N[i], theta, rho)
-        for i in range(U_coeff.size)
-        )
+        K_coeff[i] * U(L[i], N[i], theta, rho)
+        for i in range(K_coeff.size)
+        ) * 2 * np.pi
 
     return phi
 
 
 def cart2pol(x, y):
     """
-    Transformation for the cartesian coord. to polars. It is needed for the aperture function.
+    Transformation for the cartesian coord. to polars. It is needed for the
+    aperture function.
 
     Parameters
     ----------
@@ -231,12 +283,13 @@ def cart2pol(x, y):
 
     rho = np.sqrt(x ** 2 + y ** 2)  # radius normalization
     theta = np.arctan2(y, x)
+
     return rho, theta
 
 
-def aperture(x, y, U_coeff, d_z, n, i_coeff, illum):
+def aperture(x, y, K_coeff, d_z, I_coeff, illum):
     """
-    Aperture function as expresses in the paper. A multiplication from other functions.
+    Aperture function. Multiplication between the antenna truncation, the illumination function and the aberration.
     """
     r, t = cart2pol(x, y)
 
@@ -245,18 +298,21 @@ def aperture(x, y, U_coeff, d_z, n, i_coeff, illum):
     # It needs to be normalized to be orthogonal undet the Zernike polynomials
     r_norm = r / pr
 
-    _phi = phi(theta=t, rho=r_norm, U_coeff=U_coeff, n=n)
+    _phi = phi(theta=t, rho=r_norm, K_coeff=K_coeff)
     _delta = delta(x, y, d_z=d_z)
     _shape = antenna_shape(x, y)
 
-    if illum == 'gauss':
-        _illum = illumination_gauss(x, y, i_coeff=i_coeff)
-    if illum == 'pedestal':
-        _illum = illumination_pedestal(x, y, i_coeff=i_coeff)
-    if illum == 'nikolic':
-        _illum = illumination_nikolic(x, y, i_coeff=i_coeff)
+    # Wavefront aberration distribution (rad)
+    wavefront = (_phi + _delta)
 
-    E = _shape * _illum * np.exp((_phi + _delta) * 1j)
+    if illum == 'gauss':
+        _illum = illumination_gauss(x, y, I_coeff=I_coeff)
+    if illum == 'pedestal':
+        _illum = illumination_pedestal(x, y, I_coeff=I_coeff)
+    if illum == 'nikolic':
+        _illum = illumination_nikolic(x, y, I_coeff=I_coeff)
+
+    E = _shape * _illum * np.exp(wavefront * 1j)
     # Aperture: E(x/lam, y/lam)
 
     return E
@@ -269,23 +325,33 @@ def wavevector_to_degree(x, lam):
     return np.degrees(x * lam)
 
 
+def angular_spectrum(K_coeff, I_coeff, d_z, illum):
 
-def angular_spectrum(x, y, U_coeff, d_z, n, i_coeff, illum):
+    # Arrays to generate angular spectrum model
+    box_size = 500
+    x = np.linspace(-box_size, box_size, 2 ** 10)
+    y = np.linspace(-box_size, box_size, 2 ** 10)
 
-    # the input (x,y) must be a linear array, not mesh!
     dx = x[1] - x[0]
     dy = y[1] - y[0]
 
     # Normalization
-    Nx, Ny = x.size, y.size
+    # Nx, Ny = x.size, y.size
 
     # fft2 doesn't work without a grid
     x_grid, y_grid = np.meshgrid(x, y)
 
-    _aperture = aperture(x_grid, y_grid, U_coeff=U_coeff, d_z=d_z, n=n, i_coeff=i_coeff, illum=illum)
+    _aperture = aperture(
+        x=x_grid,
+        y=y_grid,
+        K_coeff=K_coeff,
+        d_z=d_z,
+        I_coeff=I_coeff,
+        illum=illum
+        )
 
-    # FFT plus normalization
-    F = np.fft.fft2(_aperture, norm='ortho') * 4 / np.sqrt(Nx * Ny)
+    # FFT, normalisation not needed, comparing normalised beam
+    F = np.fft.fft2(_aperture)  # * 4 / np.sqrt(Nx * Ny) # Normalisation
     F_shift = np.fft.fftshift(F)
 
     u, v = np.fft.fftfreq(x.size, dx), np.fft.fftfreq(y.size, dy)
