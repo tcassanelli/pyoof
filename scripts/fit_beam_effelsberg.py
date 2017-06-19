8ff4ddd937c783a30cdd06342993c1fa438a1ba7
Tomas Cassanelli <tcassanelli@gmail.com>
2017-06-19 12:33:51 +0200

    minor changes

--- scripts/fit_beam_effelsberg.py
+++ scripts/fit_beam_effelsberg.py
@@ -35,6 +35,8 @@ def fit_beam_effelsberg(pathfits):
 if __name__ == '__main__':
 
     import glob  # to list as a string files in a directory
+
+    # Change to desired data location in your machine
     observation = glob.glob('../../data/S9mm/*.fits')  # len = 8
 
     fit_beam_effelsberg(observation[0])
