b1f539ecbdec888a42895216f4b82065e07c95a6
Tomas Cassanelli <tcassanelli@gmail.com>
2017-06-19 12:19:47 +0200

    extra loop for maximum order

--- pyoof/fit_beam.py
+++ pyoof/fit_beam.py
@@ -132,12 +132,12 @@ def params_complete(params, idx, N_K_coeff):
 
 # Insert path for the fits file with pre-calibration
 def fit_beam(
-    data, order, illumination, telescope, fit_previous, resolution, angle
+    data, order_max, illumination, telescope, fit_previous, resolution, angle
         ):
 
     start_time = time.time()
 
-    print('\n ####### OOF FIT POWER PATTERN ####### \n')
+    print('\n ******* OOF FIT POWER PATTERN ******* \n')
     print('... Reading data ... \n')
 
     # All observed data needed to fit the beam
@@ -149,188 +149,194 @@ def fit_beam(
     [blockage, pr, tel_name] = telescope
     telgeo = [blockage, pr]
 
+    # Storing files in OOF_out directory
+    # pthto: path or directory where the fits file is located
+    if not os.path.exists(pthto + '/OOF_out'):
+        os.makedirs(pthto + '/OOF_out')
+
+    for j in ["%03d" % i for i in range(101)]:
+        name_dir = pthto + '/OOF_out/' + name + '-' + str(j)
+        if not os.path.exists(name_dir):
+            os.makedirs(name_dir)
+            break
+
+    print('Maximum order to be fitted: ', order_max)
     print('Telescope name: ', tel_name)
     print('File name: ', name)
     print('Observed frequency: ', freq, 'Hz')
     print('Wavelenght : ', wavel, 'm')
     print('d_z (out-of-focus): ', d_z_m, 'm')
-    print('Order n to be fitted: ', order)
     print('Illumination to be fitted: ', illum_name)
 
-    # Setting limits for plotting fitted beam
-    plim_u = [np.min(u_data[0]), np.max(u_data[0])]  # input data in radians
-    plim_v = [np.min(v_data[0]), np.max(v_data[0])]
-    plim_rad = np.array(plim_u + plim_v)
+    for order in range(1, order_max):
 
-    # d_z is given in units of wavelength (m/m)
-    d_z = np.array(d_z_m) * 2 * np.pi / wavel  # convert to radians
+        print('\n... Fit order ' + str(order) + ' ... \n')
 
-    # Beam normalisation
-    beam_data_norm = [beam_data[i] / beam_data[i].max() for i in range(3)]
+        # Setting limits for plotting fitted beam
+        # input data in radians
+        plim_u = [np.min(u_data[0]), np.max(u_data[0])]
+        plim_v = [np.min(v_data[0]), np.max(v_data[0])]
+        plim_rad = np.array(plim_u + plim_v)
 
-    n = order  # order polynomial to fit
-    N_K_coeff = (n + 1) * (n + 2) // 2  # number of Zernike coeff to fit
+        # d_z is given in units of wavelength (m/m)
+        d_z = np.array(d_z_m) * 2 * np.pi / wavel  # convert to radians
 
-    # Storing files in OOF_out directory
-    name_dir = pthto + '/OOF_out/' + name
-    # pthto: path or directory where the fits file is located
+        # Beam normalisation
+        beam_data_norm = [beam_data[i] / beam_data[i].max() for i in range(3)]
 
-    if not os.path.exists(pthto + '/OOF_out'):
-        os.makedirs(pthto + '/OOF_out')
-    if not os.path.exists(name_dir):
-        os.makedirs(name_dir)
+        n = order  # order polynomial to fit
+        N_K_coeff = (n + 1) * (n + 2) // 2  # number of Zernike coeff to fit
 
-    # Looking for result parameters lower order
-    if fit_previous and n != 1:
-        N_K_coeff_previous = n * (n + 1) // 2
-        path_params_previous = name_dir + '/fitpar_n' + str(n - 1) + '.dat'
-        params_to_add = N_K_coeff - N_K_coeff_previous
+        # Looking for result parameters lower order
+        if fit_previous and n != 1:
+            N_K_coeff_previous = n * (n + 1) // 2
+            path_params_previous = name_dir + '/fitpar_n' + str(n - 1) + '.dat'
+            params_to_add = N_K_coeff - N_K_coeff_previous
 
-        if os.path.exists(path_params_previous):
-            params_init = np.hstack(
-                (ascii.read(path_params_previous)['parfit'],
-                    np.ones(params_to_add) * 0.1)
-                )
-            print('Using initial params from n=' + str(n - 1) + ' fit')
+            if os.path.exists(path_params_previous):
+                params_init = np.hstack(
+                    (ascii.read(path_params_previous)['parfit'],
+                        np.ones(params_to_add) * 0.1)
+                    )
+                print('Initial params: n=' + str(n - 1) + ' fit')
+            else:
+                print(
+                    '\n ERROR: There is no previous parameters file fitpar_n' +
+                    str(n - 1) + '.dat in directory \n'
+                    )
         else:
-            print(
-                '\n ERROR: There is no previous parameters file fitpar_n' +
-                str(n - 1) + '.dat in directory \n'
+            params_init = np.array(
+                config_params['params_init'] + [0.1] * (N_K_coeff - 1)
                 )
-    else:
-        params_init = np.array(
-            config_params['params_init'] + [0.1] * (N_K_coeff - 1)
+            print('Initial params: default')
+            # i_amp, sigma_r, x0, y0, K(l,m)
+            # Giving an initial value of 0.1 for each coeff
+
+        bounds_min = np.array(
+            config_params['params_bounds_min'] + [-5] * (N_K_coeff - 1)
             )
-        print('Using standard initial params')
-        # i_amp, sigma_r, x0, y0, K(l,m)
-        # Giving an initial value of 0.1 for each coeff
+        bounds_max = np.array(
+            config_params['params_bounds_max'] + [5] * (N_K_coeff - 1)
+            )
 
-    bounds_min = np.array(
-        config_params['params_bounds_min'] + [-5] * (N_K_coeff - 1)
-        )
-    bounds_max = np.array(
-        config_params['params_bounds_max'] + [5] * (N_K_coeff - 1)
-        )
+        idx = config_params['params_excluded']  # exclude params from fit
+        # [0, 1, 2, 3, 4] = [i_amp, c_dB, x0, y0, K(0, 0)]
+        # or 'None' to include all
 
-    idx = config_params['params_excluded']  # exclude params from fit
-    # [0, 1, 2, 3, 4] = [i_amp, c_dB, x0, y0, K(0, 0)] or 'None' to include all
+        params_init_true = np.delete(params_init, idx)
+        bounds_min_true = np.delete(bounds_min, idx)
+        bounds_max_true = np.delete(bounds_max, idx)
 
-    params_init_true = np.delete(params_init, idx)
-    bounds_min_true = np.delete(bounds_min, idx)
-    bounds_max_true = np.delete(bounds_max, idx)
+        print('Parameters to fit: ' + str(len(params_init_true)) + '\n')
 
-    print(
-        '\n... Starting fit for ' + str(len(params_init_true)) +
-        ' parameters ... \n'
-        )
+        # Running non-linear least-squared optimization
+        res_lsq = optimize.least_squares(
+            fun=residual,
+            x0=params_init_true,
+            # conserve the same order of the arguments as the residual func
+            args=(
+                idx,
+                N_K_coeff,
+                beam_data_norm,
+                u_data,
+                v_data,
+                d_z,
+                wavel,
+                resolution,
+                illum_func,
+                telgeo,
+                True  # Grid interpolation
+                ),
+            bounds=tuple([bounds_min_true, bounds_max_true]),
+            method='trf',
+            verbose=2,
+            max_nfev=None
+            )
 
-    # Running non-linear least-squared optimization
-    res_lsq = optimize.least_squares(
-        fun=residual,
-        x0=params_init_true,
-        # conserve the same order of the arguments as the residual func
-        args=(
-            idx,
-            N_K_coeff,
-            beam_data_norm,
-            u_data,
-            v_data,
-            d_z,
-            wavel,
-            resolution,
-            illum_func,
-            telgeo,
-            True  # Grid interpolation
-            ),
-        bounds=tuple([bounds_min_true, bounds_max_true]),
-        method='trf',
-        verbose=2,
-        max_nfev=None
-        )
+        print('\n')
 
-    print('\n')
+        # Solutions from least squared optimisation
+        params_solution = params_complete(res_lsq.x, idx, N_K_coeff)
+        params_init = params_init
+        res_optim = res_lsq.fun.reshape(3, -1)  # Optimum residual from fitting
+        jac_optim = res_lsq.jac
+        grad_optim = res_lsq.grad
 
-    # Solutions from least squared optimisation
-    params_solution = params_complete(res_lsq.x, idx, N_K_coeff)
-    params_init = params_init
-    res_optim = res_lsq.fun.reshape(3, -1)  # Optimum residual from fitting
-    jac_optim = res_lsq.jac
-    grad_optim = res_lsq.grad
+        cov, corr = co_matrices(
+            res=res_lsq.fun,
+            jac=res_lsq.jac,
+            n_pars=params_init_true.size  # num of parameters fitted
+            )
+        cov_ptrue = np.vstack((np.delete(np.arange(N_K_coeff + 4), idx), cov))
+        corr_ptrue = np.vstack(
+            (np.delete(np.arange(N_K_coeff + 4), idx), corr)
+            )
 
-    cov, corr = co_matrices(
-        res=res_lsq.fun,
-        jac=res_lsq.jac,
-        n_pars=params_init_true.size  # num of parameters fitted
-        )
-    cov_ptrue = np.vstack((np.delete(np.arange(N_K_coeff + 4), idx), cov))
-    corr_ptrue = np.vstack((np.delete(np.arange(N_K_coeff + 4), idx), corr))
+        # Final phase from fit in the telescope's primary reflector
+        _phase = phase(
+            K_coeff=params_solution[4:],
+            notilt=True,
+            pr=pr
+            )
 
-    # Final phase from fit in the telescope's primary reflector
-    _phase = phase(
-        K_coeff=params_solution[4:],
-        notilt=True,
-        pr=pr
-        )
+        # Making nice table :)
+        ln = [(j, i) for i in range(0, n + 1) for j in range(-i, i + 1, 2)]
+        L = np.array(ln)[:, 0]
+        N = np.array(ln)[:, 1]
 
-    # Making nice table :)
-    ln = [(j, i) for i in range(0, n + 1) for j in range(-i, i + 1, 2)]
-    L = np.array(ln)[:, 0]
-    N = np.array(ln)[:, 1]
+        params_names = ['i_amp', taper_name, 'x_0', 'y_0']
+        for i in range(N_K_coeff):
+            params_names.append('K(' + str(L[i]) + ',' + str(N[i]) + ')')
 
-    params_names = ['i_amp', taper_name, 'x_0', 'y_0']
-    for i in range(N_K_coeff):
-        params_names.append('K(' + str(L[i]) + ',' + str(N[i]) + ')')
+        params_to_save = [params_names, params_solution, params_init]
+        info_to_save = [
+            [tel_name], [name], [d_z_m[0]], [d_z_m[1]], [d_z_m[2]], [wavel],
+            [freq], [illum_name], [meanel], [resolution]
+            ]
 
-    params_to_save = [params_names, params_solution, params_init]
-    info_to_save = [
-        [tel_name], [name], [d_z_m[0]], [d_z_m[1]], [d_z_m[2]], [wavel],
-        [freq], [illum_name], [meanel], [resolution]
-        ]
+        # Storing files in directory
+        print('... Saving data ... \n')
 
-    # Storing files in directory
-    print('... Saving data ... \n')
+        # To store fit information and found parameters in ascii file
+        store_ascii(
+            name=name,
+            order=n,
+            name_dir=name_dir,
+            params_to_save=params_to_save,
+            info_to_save=info_to_save
+            )
 
-    # To store fit information and found parameters in ascii file
-    store_ascii(
-        name=name,
-        order=n,
-        name_dir=name_dir,
-        params_to_save=params_to_save,
-        info_to_save=info_to_save
-        )
+        # To store large files in csv format
+        save_to_csv = [
+            beam_data, u_data, v_data, res_optim, jac_optim, grad_optim,
+            _phase, cov_ptrue, corr_ptrue
+            ]
 
-    # To store large files in csv format
-    save_to_csv = [
-        beam_data, u_data, v_data, res_optim, jac_optim, grad_optim, _phase,
-        cov_ptrue, corr_ptrue
-        ]
+        store_csv(
+            name=name,
+            order=n,
+            name_dir=name_dir,
+            save_to_csv=save_to_csv
+            )
 
-    store_csv(
-        name=name,
-        order=n,
-        name_dir=name_dir,
-        save_to_csv=save_to_csv
-        )
+        # Printing the results from saved ascii file
+        print(ascii.read(name_dir + '/fitpar_n' + str(n) + '.dat'))
 
-    # Printing the results from saved ascii file
-    print(ascii.read(name_dir + '/fitpar_n' + str(n) + '.dat'))
-    print('\n')
+        # Making all relevant plots
+        print('\n... Making plots ...')
 
-    # Making all relevant plots
-    print('... Making plots ... \n')
+        plot_fit_path(
+            pathoof=name_dir,
+            order=n,
+            telgeo=telgeo,
+            illum_func=illum_func,
+            plim_rad=plim_rad,
+            save=True,
+            angle=angle,
+            resolution=resolution
+            )
 
-    plot_fit_path(
-        pathoof=name_dir,
-        order=n,
-        telgeo=telgeo,
-        illum_func=illum_func,
-        plim_rad=plim_rad,
-        save=True,
-        angle=angle,
-        resolution=resolution
-        )
+        plt.close('all')
 
-    plt.close('all')
-
-    print(' ###### %s mins ######' % str((time.time() - start_time) / 60))
-    print('\n')
+    final_time = np.round((time.time() - start_time) / 60, 2)
+    print('\n **** OOF FIT COMPLETED AT ' + str(final_time) + ' mins **** \n')
