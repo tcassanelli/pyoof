0c0523ce7161837f39c064ec12e6c9f17b83b6a6
Tomas Cassanelli <tcassanelli@gmail.com>
2017-08-16 17:15:01 +0200

    erasing csv info functions

--- pyoof/aux_functions.py
+++ pyoof/aux_functions.py
@@ -6,11 +6,9 @@ import os
 from scipy.constants import c as light_speed
 from astropy.io import fits
 import numpy as np
-import csv
 
 __all__ = [
-    'extract_data_effelsberg', 'str2LaTeX', 'store_data_csv',
-    'store_info_csv', 'read_info_csv'
+    'extract_data_effelsberg', 'str2LaTeX', 'store_data_csv'
     ]
 
 
@@ -22,7 +20,7 @@ def extract_data_effelsberg(pathfits):
     Parameters
     ----------
     pathfits : str
-        Path to the fits file that contains the three beam maps precalibrated,
+        Path to the fits file that contains the three beam maps pre-calibrated,
         plus some other important parameter for the fit.
 
     Returns
@@ -31,7 +29,7 @@ def extract_data_effelsberg(pathfits):
         It contains all extra data besides the beam map.
         data_info = [name, pthto, freq, wavel, d_z, meanel].
     data_obs : list
-        It contains beam maps and x-, y-axis data for the leat squares
+        It contains beam maps and x-, y-axis data for the least squares
         optimization.
         data_obs = [beam_data, u_data, v_data].
     """
@@ -70,7 +68,7 @@ def extract_data_effelsberg(pathfits):
 
 def str2LaTeX(python_string):
     """
-    Function that solves the underscore problem in a python string to LaTeX
+    Function that solves the underscore problem in a python string to LaTeX,
     it changes it from _ -> \_ symbol. Useful in matplotlib plots.
 
     Parameters
@@ -81,7 +79,7 @@ def str2LaTeX(python_string):
     Returns
     -------
     LaTeX_string : str
-        Sring with the new underscore symbol.
+        String with the new underscore symbol.
     """
 
     string_list = list(python_string)
@@ -97,14 +95,14 @@ def str2LaTeX(python_string):
 
 def store_data_csv(name, order, name_dir, save_to_csv):
     """
-    Function that stores all important information in a csv files after the
-    least squares optimization has finished. It will be saved in the
+    Stores all important information in a csv file after the least squares
+    optimization has finished. It will be saved in the
     'OOF_out/name' directory.
 
     Parameters
     ----------
     name : str
-        File name of the fits file to be optimised.
+        File name of the fits file to be optimized.
     order : int
         Maximum order for the optimization in the Zernike circle polynomials
         coefficients.
@@ -119,7 +117,7 @@ def store_data_csv(name, order, name_dir, save_to_csv):
     """
 
     headers = [
-        'Normalised beam', 'u vector radians', 'v vector radians', 'Residual',
+        'Normalized beam', 'u vector radians', 'v vector radians', 'Residual',
         'Jacobian', 'Gradient', 'Phase primary reflector radians',
         'Variance-Covariance matrix (first row fitted parameters idx)',
         'Correlation matrix (first row fitted parameters idx)'
@@ -143,72 +141,3 @@ def store_data_csv(name, order, name_dir, save_to_csv):
             X=file,
             header=header + ' ' + name
             )
-
-
-def store_info_csv(info_dict, name_dir):
-    """
-    Stores a python dictionary in csv format.
-
-    Parameters
-    ----------
-    info_dict : dict
-        Dictionary with the relevant information to be stored.be
-    name_dir : str
-        Name of the directory where the dictionary is placed.
-    """
-
-    with open(name_dir + '/file_info.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=' ')
-        for key, value in info_dict.items():
-            writer.writerow([key, value])
-
-
-def is_number(s):
-    """
-    identifies if a string contains a number or not. Useful to transform a
-    stored dictionary.
-
-    Parameters
-    ----------
-    s : str
-
-    """
-
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
-
-    return False
-
-
-def read_info_csv(path_info):
-    """
-    Transform a csv file into a python dictionary.
-
-    Parameters
-    ----------
-    path_info : str
-        Path to the dictionary previously stored in .csv format.
-
-    Returns
-    -------
-    file_dict : dict
-    """
-
-    with open(path_info, 'r', newline='') as csv_file:
-        reader = csv.reader(csv_file, delimiter=' ')
-        file_dict = dict(reader)
-
-    for i in file_dict.keys():
-        if is_number(file_dict[i]):
-            file_dict[i] = float(file_dict[i])
-
-    return file_dict
