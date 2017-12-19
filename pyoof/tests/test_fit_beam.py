a4464a213f7c36b4937bdd68aac8ff7a245f361e
Tomas Cassanelli <tcassanelli@gmail.com>
2017-12-19 12:04:40 +0100

    testing several configurations using tempdir

--- pyoof/tests/test_fit_beam.py
+++ pyoof/tests/test_fit_beam.py
@@ -6,12 +6,12 @@ import pytest
 import os
 import numpy as np
 from astropy.io import ascii
-from numpy.testing import assert_array_almost_equal, assert_allclose
+from numpy.testing import assert_allclose
 import pyoof
 
 
 # Initial fits file configuration
-n = 3                                           # initial order
+n = 4                                           # initial order
 N_K_coeff = (n + 1) * (n + 2) // 2 - 1          # total numb. polynomials
 c_dB = np.random.randint(-21, -10)              # illumination taper
 I_coeff = np.array([1, c_dB, 0, 0])             # illumination coefficients
@@ -19,7 +19,10 @@ I_coeff = np.array([1, c_dB, 0, 0])             # illumination coefficients
 # Zernike circle polynomial coeff, first one kept fixed.
 K_coeff = np.hstack((0, np.random.normal(0., .08, N_K_coeff)))
 wavel = 0.0093685143125                         # wavelenght in m
-d_z = [0.022, 0, -0.022]                        # radial offset
+
+plus_minus = np.random.normal(0.025, 0.005)
+d_z = [plus_minus, 0, -plus_minus]              # radial offset
+
 params_true = np.hstack((I_coeff, K_coeff))
 
 noise_level = 1e1                               # noise added to gen data
@@ -32,14 +35,13 @@ effelsberg_telescope = [
     ]
 
 # Least squares minimization
-method = 'trf'
 resolution = 2 ** 8
 box_factor = 5
 
 
 # Generating temp file with pyoof fits and pyoof_out
-@pytest.fixture
-def oof_work_dir(tmpdir_factory):
+@pytest.fixture(params=['trf', 'lm', 'dogbox'])
+def oof_work_dir(tmpdir_factory, request):
 
     tdir = str(tmpdir_factory.mktemp('pyoof'))
 
@@ -56,15 +58,17 @@ def oof_work_dir(tmpdir_factory):
         )
 
     print('files directory: ', tdir)
+    print('method tested: ', request.param)
 
     # Reading the generated data
+
     pathfits = os.path.join(tdir, 'data_generated', 'test000.fits')
     data_info, data_obs = pyoof.extract_data_pyoof(pathfits)
 
     pyoof.fit_beam(
         data_info=data_info,
         data_obs=data_obs,
-        method=method,
+        method=request.param,
         order_max=n,
         illum_func=pyoof.aperture.illum_pedestal,
         telescope=effelsberg_telescope,
@@ -87,10 +91,9 @@ def test_fit_beam(oof_work_dir):
 
     # lets compare the params from the last order
     fit_pars = os.path.join(
-        oof_work_dir, 'pyoof_out', 'test000-000',
-        'fitpar_n{}.csv'.format(n)
+        oof_work_dir, 'pyoof_out', 'test000-000', 'fitpar_n{}.csv'.format(n)
         )
 
     params = ascii.read(fit_pars)['parfit']
 
-    assert_array_almost_equal(params, params_true, decimal=2)
+    assert_allclose(params, params_true, rtol=1e-2, atol=1e-2)
