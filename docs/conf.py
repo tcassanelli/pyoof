1a10c34e23ab259b62337eb2a0d30dfebc78f9e6
Tomas Cassanelli <tcassanelli@gmail.com>
2019-09-11 11:57:44 -0400

    debugging docs

--- docs/conf.py
+++ docs/conf.py
@@ -96,15 +96,23 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg['name']
+project = setup_cfg['package_name']
 author = setup_cfg['author']
-copyright = '2019, The pyoof developers'
-version = setup_cfg['version']
-release = setup_cfg['version']
+copyright = '{0}, {1}'.format(
+    datetime.datetime.now().year, setup_cfg['author'])
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
+
+__import__(setup_cfg['package_name'])
+package = sys.modules[setup_cfg['package_name']]
+
+# The short X.Y version.
+version = package.__version__.split('-', 1)[0]
+# The full version, including alpha/beta/rc tags.
+release = package.__version__
+
 
 # -- Options for HTML output --------------------------------------------------
 
@@ -179,18 +187,18 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 
 # -- Options for the edit_on_github extension ---------------------------------
 
-# if eval(setup_cfg.get('edit_on_github')):
-#     extensions += ['sphinx_astropy.ext.edit_on_github']
+if eval(setup_cfg.get('edit_on_github')):
+    extensions += ['sphinx_astropy.ext.edit_on_github']
 
-#     versionmod = __import__(project + '.version')
-#     edit_on_github_project = 'tcassanelli/pyoof'
-#     if versionmod.version.release:
-#         edit_on_github_branch = "v" + versionmod.version.version
-#     else:
-#         edit_on_github_branch = "master"
+    versionmod = __import__(setup_cfg['package_name'] + '.version')
+    edit_on_github_project = setup_cfg['github_project']
+    if versionmod.version.release:
+        edit_on_github_branch = "v" + versionmod.version.version
+    else:
+        edit_on_github_branch = "master"
 
-#     edit_on_github_source_root = ""
-#     edit_on_github_doc_root = "docs"
+    edit_on_github_source_root = ""
+    edit_on_github_doc_root = "docs"
 
 # -- Resolving issue number to links in changelog -----------------------------
 github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_project'])
