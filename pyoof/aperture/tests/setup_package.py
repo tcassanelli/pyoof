d1b62f93e9bb9ee6098abc90b1c887c8bfc30722
Tomas Cassanelli <tcassanelli@gmail.com>
2019-09-13 09:56:11 -0400

    debugging travis ci and docs

--- pyoof/aperture/tests/setup_package.py
+++ pyoof/aperture/tests/setup_package.py
@@ -1,2 +1,5 @@
+from __future__ import absolute_import
+
+
 def get_package_data():
     return {'pyoof.aperture.tests': ['data/*.*']}
--- pyoof/zernike/tests/setup_package.py
+++ pyoof/zernike/tests/setup_package.py
@@ -1,2 +1,5 @@
+from __future__ import absolute_import
+
+
 def get_package_data():
     return {'pyoof.zernike.tests': ['data/*.*']}
