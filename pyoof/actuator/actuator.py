fixing normalization type

pyoof/actuator/actuator.py
@@ -4,7 +4,7 @@
 # Author: Tomas Cassanelli
 
 """
-This is a developer module at the moment and only Effeslberg functions.
+This is a developer module at the moment and only Effelsberg functions.
 """
 
 import os
@@ -38,7 +38,7 @@ plt.style.use(get_pkg_data_filename('../data/pyoof.mplstyle'))
 def generate_actuators(edge):
     """
     This function generates the actuators coordinates for the sub-reflector at
-    the Effelsberg telecope. Since edges of the pyoof output are not well
+    the Effelsberg telescope. Since edges of the `~pyoof` output are not well
     constrained (least squares minimization), the ``edge`` argument will
     subtract ``edge`` (in length units) from the external ring of the actuators
     position.
@@ -105,7 +105,7 @@ def actuator_displacement(
     """
     Calculates displacement for the active surface at the Effelsberg telescope.
     Given the phase error (output from `~pyoof.fit_zpoly`), the phase errors
-    in the sub-reflector can be approximated and specified as perpendicualar
+    in the sub-reflector can be approximated and specified as perpendicular
     displacement for a set of 92 actuators in the current active surface.
     """
 
@@ -117,7 +117,7 @@ def actuator_displacement(
         )
     phase = phase * apu.rad  # stored as default radians
 
-    pr = 50 * apu.m    # Primary refelctor
+    pr = 50 * apu.m    # Primary reflector
     sr = 3.25 * apu.m  # sub-reflector radius
     x = np.linspace(-sr, sr, phase.shape[0])
     y = x.copy()
@@ -159,7 +159,7 @@ def actuator_displacement(
         act_data['name'],              # Label actuator as in Effelsberg sr
         act_data['x'], act_data['y'],  # coordinates actuators [mm]
         act_phase.to_value(apu.rad),   # Phase-error
-        act_phase_ad.to_value(apu.um)  # Phase to perpendicualr displacement
+        act_phase_ad.to_value(apu.um)  # Phase to perpendicular displacement
         ]
 
     print(
@@ -229,7 +229,7 @@ def plot_actuator_displacement(
     """
     Plot for the phase-error given the actuators in the active surface.
     Translates the phase-error from the whole telescope to an approximated
-    version at the sub-refelctor (at the moment only for Effelsberg).
+    version at the sub-reflector (at the moment only for Effelsberg).
     This will also calculates the amplitude value un the actuators mesh.
     """
 

pyoof/tests/test_fit_beam.py
@@ -12,7 +12,7 @@ import pyoof
 
 
 # Initial fits file configuration
-n = 6                                           # initial order
+n = 5                                           # initial order
 N_K_coeff = (n + 1) * (n + 2) // 2 - 1          # total numb. polynomials
 c_dB = np.random.randint(-21, -10) * apu.dB     # illumination taper
 wavel = 0.0093685143125 * apu.m                 # wavelength

pyoof/tests/test_simulate_data_pyoof.py
@@ -17,7 +17,7 @@ N_K_coeff = (n + 1) * (n + 2) // 2          # total number of polynomials
 c_dB = -14 * apu.dB                         # illumination taper
 I_coeff = [1, c_dB, 0 * apu.m, 0 * apu.m]   # illumination coefficients
 K_coeff = np.array([0.1] * N_K_coeff)       # random Zernike circle coeff.
-wavel = 0.0093685143125 * apu.m             # wavelenght
+wavel = 0.0093685143125 * apu.m             # wavelength
 d_z = [2.2, 0, -2.2] * apu.cm     # radial offset
 
 # Making example for the Effelsberg telescope
@@ -61,13 +61,16 @@ def test_simulate_data_pyoof(oof_work_dir):
     [beam_data, u_data, v_data] = data_obs
 
     data_info_true, data_obs_true = pyoof.extract_data_pyoof(
-        get_pkg_data_filename('data/beam_simulated.fits')
+        get_pkg_data_filename('data/data_simulated.fits')
         )
     [beam_data_true, u_data_true, v_data_true] = data_obs_true
 
     assert_allclose(beam_data, beam_data_true)
     assert_quantity_allclose(u_data, u_data_true)
     assert_quantity_allclose(v_data, v_data_true)
-    assert_quantity_allclose(data_info[6], data_info_true[6])
-    assert data_info[2:6] == data_info_true[2:6]
+
+    for i in range(4, 7):
+        assert_quantity_allclose(data_info[i], data_info_true[i])
+
+    assert data_info[2:4] == data_info_true[2:4]
     assert data_info[7] == data_info_true[7]