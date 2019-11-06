b849a99ec0497f702512c1dc9ad5be20ad92c231
Tomas Cassanelli <tcassanelli@protonmail.com>
2019-11-06 10:29:04 -0500

    debugging .travis CI in new astropy branch

--- pyoof/conftest.py
+++ pyoof/conftest.py
@@ -1,50 +1,21 @@
-# this contains imports plugins that configure py.test for astropy tests.
-# by importing them here in conftest.py they are discoverable by py.test
-# no matter how it is invoked within the source tree.
-
-# from astropy.tests.plugins.display import *
-try:
-    from astropy.tests.helper import *
-except Exception as e:
-    from pytest_astropy_header.display import *
-
-## Uncomment the following line to treat all DeprecationWarnings as
-## exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
-## as follow (although default should work for most cases).
-## To ignore some packages that produce deprecation warnings on import
-## (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
-## 'setuptools'), add:
-##     modules_to_ignore_on_import=['module_1', 'module_2']
-## To ignore some specific deprecation warning messages for Python version
-## MAJOR.MINOR or later, add:
-##     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
-# enable_deprecations_as_exceptions()
-
-## Uncomment and customize the following lines to add/remove entries from
-## the list of packages for which version numbers are displayed when running
-## the tests. Making it pass for KeyError is essential in some cases when
-## the package uses other astropy affiliated packages.
-# try:
-#     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-#     PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
-#     del PYTEST_HEADER_MODULES['h5py']
-# except (NameError, KeyError):  # NameError is needed to support Astropy < 1.0
-#     pass
-
-# Uncomment the following lines to display the version number of the
-# package rather than the version number of Astropy in the top line when
-# running the tests.
 import os
 
-# This is to figure out the affiliated package version, rather than
-# using Astropy's
-try:
-    from .version import version
-except ImportError:
-    version = 'dev'
+from astropy.version import version as astropy_version
+if astropy_version < '3.0':
+    from astropy.tests.pytest_plugins import *
+    del pytest_report_header
+else:
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 
-try:
-    pyoof = os.path.basename(os.path.dirname(__file__))
-    TESTED_VERSIONS[pyoof] = version
-except NameError:   # Needed to support Astropy <= 1.0.0
-    pass
+
+def pytest_configure(config):
+
+    config.option.astropy_header = True
+
+    PYTEST_HEADER_MODULES.pop('Pandas', None)
+    PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+
+    from .version import version, astropy_helpers_version
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version
+    TESTED_VERSIONS['astropy_helpers'] = astropy_helpers_version
