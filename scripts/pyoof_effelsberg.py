bac11f5515ca40537b9e4524c59578f978dcf017
Tomas Cassanelli <tcassanelli@protonmail.com>
2020-09-30 17:48:12 -0400

    corrections after Effelsberg blockage update

--- scripts/pyoof_effelsberg.py
+++ scripts/pyoof_effelsberg.py
@@ -47,35 +47,29 @@ def compute_phase_error(pathfits, order_max):
     [name, pthto, obs_object, obs_date, freq, wavel, d_z, meanel] = data_info
     [beam_data, u_data, v_data] = data_obs
 
-    fit_zpoly(
-        data_info=data_info,
-        data_obs=[beam_data, u_data, v_data],
-        order_max=order_max,                 # it'll fit from 1 to order_max
-        # illum_func=aperture.illum_pedestal,  # or illum_gauss
-        illum_func=aperture.illum_gauss,
-        telescope=telescope['effelsberg_20deg'],
-        fit_previous=True,                    # True is recommended
-        resolution=2 ** 9,                   # standard is 2 ** 8
-        box_factor=5,              # box_size = 5 * pr, better pixel resolution
-        config_params_file=None,   # default or add path config_file.yaml
-        make_plots=True,           # for now testing only the software
-        verbose=2,
-        work_dir=None
-        # work_dir='/scratch/v/vanderli/cassane'
-        )
+    for configuration in telescope.keys():
+        fit_zpoly(
+            data_info=data_info,
+            data_obs=[beam_data, u_data, v_data],
+            order_max=order_max,
+            illum_func=aperture.illum_pedestal,
+            # illum_func=aperture.illum_gauss,
+            telescope=telescope[configuration],
+            fit_previous=True,                   # True is recommended
+            resolution=2 ** 9,         # standard is 2 ** 8
+            box_factor=5,              # box_size = 5 * pr
+            config_params_file=None,   # default or add path config_file.yaml
+            make_plots=True,           # for now testing only the software
+            verbose=2,
+            work_dir=None
+            # work_dir='/scratch/v/vanderli/cassane'
+            )
 
 
 if __name__ == '__main__':
     # pth2data = '/Users/tomascassanelli/MPIfR/OOF/data2020/Sep2020/*.fits'
-    # files = glob.glob(pth2data)
-
-
-    pth2data = '/Users/tomascassanelli/MPIfR/OOF/data2020/Sep2020/3C454.3_32deg_3974-3981_L.fits'
-    files = [pth2data]
-    # pth2data = '/Users/tomascassanelli/MPIfR/OOF/data/S7mm_FEM'      # local
-    # pth2data = '/scratch/v/vanderli/cassane/pyoof_data/Sep2020/*.fits' # scinet
-    # pth2data = '/Users/tomascassanelli/MPIfR/OOF/data2017_offset/S9mm_nooffset/*.fits'
-    
+    pth2data = '/scratch/v/vanderli/cassane/pyoof_data/Sep2020/*.fits'
+    files = glob.glob(pth2data)
 
     for _f in files:
-        compute_phase_error(pathfits=_f, order_max=5)
+        compute_phase_error(pathfits=_f, order_max=6)
