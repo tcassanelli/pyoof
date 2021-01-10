80a31928674b28ff63752b197dc1948a852cccdb
Tomas Cassanelli <tcassanelli@protonmail.com>
2021-01-10 16:33:58 -0500

    minor rewording docstrings

--- pyoof/actuator/__init__.py
+++ pyoof/actuator/__init__.py
@@ -4,7 +4,7 @@
 """
 This sub-package contains tools to transform the telescope phase (as an
 optical system) to an equivalent phase in terms of an active surface control
-system that can correct such deformations in terms of actuator displacement.
+system that can correct such deformations using actuator displacement.
 """
 
 from .actuator import *
